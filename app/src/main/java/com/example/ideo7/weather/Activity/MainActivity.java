b3c15ea39ca9ca9a68724ebb8ba3e14264642a7c
jash90@users.noreply.github.com <bartekziimny90@gmail.com>
2017-07-24 22:30:38 +0200

    Orientation

--- app/src/main/java/com/example/ideo7/weather/Activity/MainActivity.java
+++ app/src/main/java/com/example/ideo7/weather/Activity/MainActivity.java
@@ -204,5 +204,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation==Configuration.ORIENTATION_LANDSCAPE){
+
+        }else if (newConfig.orientation==Configuration.ORIENTATION_PORTRAIT){
+
+        }
     }
 }
